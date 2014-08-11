07df1b2bfc720dc3fc27a695f603839f7f5c421f
esalomon <efrain_salomon@yahoo.com.mx>
2014-08-11 11:26:53 -0500

    [Dominios] Implementación de la clase semilla con arreglos.

--- src/domain/Semilla.groovy
+++ src/domain/Semilla.groovy
@@ -5,19 +5,47 @@ package domain
  */
 class Semilla {
 
+    //Atributos de la clase
     String nombre
 
-    Integer alto
-    Integer ancho
+    private Integer alto
+    private Integer ancho
 
-    Elemento elementoInicial
+    ArrayList elementos
 
-    //Constructor de los objetos semilla
-    Semilla (nombre, alto, ancho) {
+    //Constructor por defectos de los objetos semilla
+    Semilla (nombre) {
         //Se asignan los parámetros de la clase.
         this.nombre = nombre
-        this.alto   = alto
-        this.ancho  = ancho
+
+        //Definición inicial de la semilla
+        elementos = [[0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0]]
+
+        //Se obtienen el número de columnas y renglones del arreglo
+        this.alto   = elementos.size()
+        this.ancho  = elementos[0].size()
+        println ""
+    }
+
+    //Constructor por defectos de los objetos semilla
+    Semilla (nombre, elementos) {
+        //Se asignan los parámetros de la clase.
+        this.nombre = nombre
+
+        //Definición inicial de la semilla
+        this.elementos = elementos
+
+        //Se obtienen el número de columnas y renglones del arreglo
+        this.alto   = this.elementos.size()
+        this.ancho  = this.elementos[0].size()
+        println ""
     }
 
     //Representación en texto de los objetos semilla
@@ -28,17 +56,42 @@ class Semilla {
         //Se imprime el encabezdo de la semilla.
         output.append "Objeto Semilla \n"
         output.append " + nombre: $nombre \n"
-        output.append " + alto: $alto, ancho: $ancho \n"
+        output.append " +   alto: $alto\n"
+        output.append " +  ancho: $ancho \n"
+
+        //Se recorren los arreglos para obtener los valores
+        for (int renglon = 0; renglon < this.alto; renglon++ ){
+            for (int columna = 0; columna < this.ancho; columna++ ){
+
+                output.append this.elementos[renglon][columna]
+            }
+            output.append "\n"
+        }
 
         //Se regresa l representación en texto del objeto
         return output
     }
 
+    //Método principal de la clase.
     static void main(args) {
         //Se crea una semilla de prueba
-        def semilla = new Semilla('Semilla de Prueba', 8, 8)
+        def semilla1 = new Semilla('Semilla de Prueba 1')
 
         //Se imprime el contenido de la semilla de prueba
-        print semilla
+        print semilla1
+
+        //Se crea una semilla a partir de un arreglo
+        def elementos = [[0,0,0,0,0,0,0],
+                         [0,0,0,1,1,0,0],
+                         [0,0,0,0,0,0,0],
+                         [0,1,1,0,0,1,1],
+                         [0,0,0,0,0,0,0],
+                         [0,0,0,0,0,0,0],
+                         [0,0,1,1,1,1,0]]
+
+        def semilla2 = new Semilla('Semilla de Prueba 2', elementos)
+
+        //Se imprime el contenido de la semilla de prueba
+        print semilla2
     }
 }
