1086c78e96231b2d2be0f04b105508acc5f02632
esalomon <efrain_salomon@yahoo.com.mx>
2014-08-11 17:48:15 -0500

    [Utils] Se creó un método para clonar el contenido de un array list.

--- src/services/UtileriasService.groovy
+++ src/services/UtileriasService.groovy
@@ -5,6 +5,7 @@ package services
  */
 class UtileriasService {
 
+    //Para convertir un array list en una cadena.
     String arrayListToString (arrayList) {
         //Se define una variable para guardar la cadena generada.
         StringBuffer output = new StringBuffer("")
@@ -26,6 +27,7 @@ class UtileriasService {
         return output
     }
 
+    //Para eliminar los elementos de un array list
     void arrayListClear (arrayList) {
         //Se obtienen el número de columnas y renglones del arreglo
         def alto   = arrayList.size()
@@ -39,5 +41,34 @@ class UtileriasService {
 
         //Se liberan el renglon externo
         arrayList.clear()
+    }
+
+    //Para clonar el contenido de un array list
+    ArrayList arrayListClone (arrayList) {
+        //Se obtienen el número de columnas y renglones del arreglo
+        def alto   = arrayList.size()
+        def ancho  = arrayList[0].size()
+
+        //Se crea un arraylist para cada renglón
+        def arrayClone = new ArrayList()
+
+        //Se recorren los renglones del arreglo
+        for (int renglon = 0; renglon < alto; renglon++ ){
+
+            //Se crea un arraylist para cada columna
+            arrayClone[renglon] = new ArrayList()
+
+            //Se recorren las columnas del arreglo
+            for (int columna = 0; columna < ancho; columna++ ){
+
+                //Se crea un objeto nuevo con el valor del arrelgo original.
+                def elemento = new Integer(arrayList[renglon][columna])
+                //Se agrega el nuevo objeto al nuevo arreglo.
+                arrayClone[renglon][columna] = elemento
+            }
+        }
+
+        //Se regresa l representación en texto del objeto
+        return arrayClone
     }
 }
