d35f86162ce517bd57f7b3400f9a72f179bc810d
esalomon <efrain_salomon@yahoo.com.mx>
2014-08-11 11:43:52 -0500

    [Servicios] Se agregaron otros tres tipos de patrones.

--- src/services/SemillaService.groovy
+++ src/services/SemillaService.groovy
@@ -14,14 +14,74 @@ class SemillaService {
         def elementos = [[0,0,0,0,0,0,0,0],
                          [0,0,0,0,0,0,0,0],
                          [0,0,0,0,0,0,0,0],
-                         [0,0,0,0,0,0,0,0],
-                         [0,0,0,0,0,0,0,0],
+                         [0,0,0,1,1,0,0,0],
+                         [0,0,0,1,1,0,0,0],
                          [0,0,0,0,0,0,0,0],
                          [0,0,0,0,0,0,0,0],
                          [0,0,0,0,0,0,0,0]]
 
         //Se crea un objeto semilla con el arreglo definido previamente
-        def semillaBlock = new Semilla('Semilla Block', elementos)
+        def semillaBlock = new Semilla('Block', elementos)
+
+        //Se regresa la semilla recién creada
+        return semillaBlock
+    }
+
+    //Se define una semilla con patrón Block
+    def createSemillaBeehive () {
+
+        //Se define el patrón de la semilla
+        def elementos = [[0,0,0,0,0,0,0,0],
+                         [0,0,0,0,0,0,0,0],
+                         [0,0,0,1,1,0,0,0],
+                         [0,0,1,0,0,1,0,0],
+                         [0,0,0,1,1,0,0,0],
+                         [0,0,0,0,0,0,0,0],
+                         [0,0,0,0,0,0,0,0],
+                         [0,0,0,0,0,0,0,0]]
+
+        //Se crea un objeto semilla con el arreglo definido previamente
+        def semillaBlock = new Semilla('Beehive', elementos)
+
+        //Se regresa la semilla recién creada
+        return semillaBlock
+    }
+
+    //Se define una semilla con patrón Block
+    def createSemillaLoaf () {
+
+        //Se define el patrón de la semilla
+        def elementos = [[0,0,0,0,0,0,0,0],
+                         [0,0,0,0,0,0,0,0],
+                         [0,0,0,0,0,0,0,0],
+                         [0,0,0,1,1,0,0,0],
+                         [0,0,1,0,0,1,0,0],
+                         [0,0,0,1,0,1,0,0],
+                         [0,0,0,0,1,0,0,0],
+                         [0,0,0,0,0,0,0,0]]
+
+        //Se crea un objeto semilla con el arreglo definido previamente
+        def semillaBlock = new Semilla('Loaf', elementos)
+
+        //Se regresa la semilla recién creada
+        return semillaBlock
+    }
+
+    //Se define una semilla con patrón Block
+    def createSemillaBoat () {
+
+        //Se define el patrón de la semilla
+        def elementos = [[0,0,0,0,0,0,0,0],
+                         [0,0,0,0,0,0,0,0],
+                         [0,0,0,0,0,0,0,0],
+                         [0,0,1,1,0,0,0,0],
+                         [0,0,1,0,1,0,0,0],
+                         [0,0,0,1,0,0,0,0],
+                         [0,0,0,0,0,0,0,0],
+                         [0,0,0,0,0,0,0,0]]
+
+        //Se crea un objeto semilla con el arreglo definido previamente
+        def semillaBlock = new Semilla('Boat', elementos)
 
         //Se regresa la semilla recién creada
         return semillaBlock
@@ -30,13 +90,24 @@ class SemillaService {
     //Pruebas de los métodos de la clase
     static void main(args) {
 
-        //Se define una instancia de la clase
+        //Se define las variables del médoto
         def service = new SemillaService()
+        def semilla
 
         //Se ejecuta el método a ser probado y se imprime su contenido.
-        def semilla = service.createSemillaBlock()
+        semilla = service.createSemillaBlock()
         println semilla
 
+        //Se ejecuta el método a ser probado y se imprime su contenido.
+        semilla = service.createSemillaBeehive()
+        println semilla
 
+        //Se ejecuta el método a ser probado y se imprime su contenido.
+        semilla = service.createSemillaLoaf()
+        println semilla
+
+        //Se ejecuta el método a ser probado y se imprime su contenido.
+        semilla = service.createSemillaBoat()
+        println semilla
     }
 }
