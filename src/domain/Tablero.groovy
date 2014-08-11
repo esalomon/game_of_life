b5eb24cc6d276b0a0fb49aae62ffa13c3d3d1d8e
esalomon <efrain_salomon@yahoo.com.mx>
2014-08-11 17:53:54 -0500

    [Domino] Se implementa el código que realiza los cálculos de la siguiente generación.

--- src/domain/Tablero.groovy
+++ src/domain/Tablero.groovy
@@ -18,16 +18,16 @@ class Tablero {
 
     ArrayList elementos
 
-    Tablero (Semilla semilla) {
+    Tablero(Semilla semilla) {
         //Se asignan los parámetros de la clase.
         this.semilla = semilla
 
         //Se copia el arreglo de la semilla al arreglo del tablero.
-        elementos = semilla.elementos.clone()
+        elementos = utilerias.arrayListClone(semilla.elementos)
 
         //Se obtienen el número de columnas y renglones del arreglo
-        this.alto   = elementos.size()
-        this.ancho  = elementos[0].size()
+        this.alto = elementos.size()
+        this.ancho = elementos[0].size()
     }
 
     //Destrucción del objeto semilla
@@ -37,19 +37,19 @@ class Tablero {
 
         //Se asigna null a las variables de la instancia
         semilla = null
-        alto    = null
-        ancho   = null
+        alto = null
+        ancho = null
     }
 
     //Representación en texto de los objetos tablero
-    String toString (){
+    String toString() {
         //Se define una variable para guardar la cadena generada.
         StringBuffer output = new StringBuffer("")
 
         //Se imprime el encabezdo de la semilla.
         output.append "Objeto Tablero \n"
-        output.append " +   alto:  $alto\n"
-        output.append " +  ancho:  $ancho \n"
+        output.append " +    alto:  $alto\n"
+        output.append " +   ancho:  $ancho \n"
         output.append " + semilla: $semilla.nombre \n"
 
         //Se recorren los arreglos para obtener los valores
@@ -59,14 +59,237 @@ class Tablero {
         return output
     }
 
+    //Se recorre el arreglo calculando el nuevo estado de cada celula.
+    void calcularNuevoEstado() {
+        def suma
+
+        //Se genera una copia temporal del arreglo de elementos
+        def arrelgoCopia = utilerias.arrayListClone(elementos)
+
+        //Se recorren los arreglos para obtener los valores
+        for (int renglon = 0; renglon < alto; renglon++) {
+            for (int columna = 0; columna < ancho; columna++) {
+
+                //Se suma el contenido de las celdas.
+                suma = calcularSumaCeldas(arrelgoCopia, renglon, columna)
+
+                //Si el resultado de la suma es 3 el estado de la celula sera víva.
+                if (suma == 3) {
+                    elementos[renglon][columna] = 1
+                }
+
+                //Si el resultado de la suma es 4 entonces se concerva el estado anterior
+                else if (suma == 4) {
+                    elementos[renglon][columna] = arrelgoCopia[renglon][columna]
+                }
+
+                //Cualquier otro resultado implica que la celda muere.
+                else {
+                    elementos[renglon][columna] = 0
+                }
+            }
+        }
+
+        //Se libera la memoria asociada a la copia
+        utilerias.arrayListClear(arrelgoCopia)
+    }
+
+    //Se suman los elementos vivos de cada grupo.
+    int calcularSumaCeldas(arrayList, renglon, columna) {
+        //Inicialización de la variable suma
+        def suma = 0
+
+        //Se obtiene el valor de cada una de las nueve celdas
+        suma += getNorte1(arrayList, renglon, columna)
+        suma += getNorte2(arrayList, renglon, columna)
+        suma += getNorte3(arrayList, renglon, columna)
+        suma += getOeste1(arrayList, renglon, columna)
+        suma += arrayList[renglon][columna]
+        suma += getEste1(arrayList, renglon, columna)
+        suma += getSur1(arrayList, renglon, columna)
+        suma += getSur2(arrayList, renglon, columna)
+        suma += getSur3(arrayList, renglon, columna)
+
+        //Se regresa el cálculo de la suma
+        return suma
+    }
+
+    private int getNorte1(arrayList, renglon, columna) {
+        //Definición de las variables auxiliares
+        def indexRow, indexColumn
+
+        //Se obtiene el índice del renglón norte
+        indexRow = getRenglonNorte(renglon)
+
+        //Se obtiene el índice de la columna oeste
+        indexColumn = getColumnaOeste(columna)
+
+        //Se regresa el contenido de la posición solicitada
+        return arrayList[indexRow][indexColumn]
+    }
+
+    private int getNorte2(arrayList, renglon, columna) {
+        //Definición de la variable auxiliar
+        def indexRow
+
+        //Se obtiene el índice del renglón norte
+        indexRow = getRenglonNorte(renglon)
+
+        //Se regresa el contenido de la posición solicitada
+        return arrayList[indexRow][columna]
+    }
+
+    private int getNorte3(arrayList, renglon, columna) {
+        //Definición de las variables auxiliares
+        def indexRow, indexColumn
+
+        //Se obtiene el índice del renglón norte
+        indexRow = getRenglonNorte(renglon)
+
+        //Se obtiene el índice de la columna este
+        indexColumn = getColumnaEste(columna)
+
+        //Se regresa el contenido de la posición solicitada
+        return arrayList[indexRow][indexColumn]
+    }
+
+    private int getOeste1(arrayList, renglon, columna) {
+        //Definición de la variable auxiliar
+        def indexColumn
+
+        //Se obtiene el índice de la columna oeste
+        indexColumn = getColumnaOeste(columna)
+
+        //Se regresa el contenido de la posición solicitada
+        return arrayList[renglon][indexColumn]
+    }
+
+    private int getEste1(arrayList, renglon, columna) {
+        //Definición de la variable auxiliar
+        def indexColumn
+
+        //Se obtiene el índice de la columna este
+        indexColumn = getColumnaEste(columna)
+
+        //Se regresa el contenido de la posición solicitada
+        return arrayList[renglon][indexColumn]
+    }
+
+    private int getSur1(arrayList, renglon, columna) {
+        //Definición de las variables auxiliares
+        def indexRow, indexColumn
+
+        //Se obtiene el índice del renglón norte
+        indexRow = getRenglonSur(renglon)
+
+        //Se obtiene el índice de la columna oeste
+        indexColumn = getColumnaOeste(columna)
+
+        //Se regresa el contenido de la posición solicitada
+        return arrayList[indexRow][indexColumn]
+    }
+
+    private int getSur2(arrayList, renglon, columna) {
+        //Definición de la variable auxiliar
+        def indexRow
+
+        //Se obtiene el índice del renglón norte
+        indexRow = getRenglonSur(renglon)
+
+        //Se regresa el contenido de la posición solicitada
+        return arrayList[indexRow][columna]
+    }
+
+    private int getSur3(arrayList, renglon, columna) {
+        //Definición de las variables auxiliares
+        def indexRow, indexColumn
+
+        //Se obtiene el índice del renglón norte
+        indexRow = getRenglonSur(renglon)
+
+        //Se obtiene el índice de la columna este
+        indexColumn = getColumnaEste(columna)
+
+        //Se regresa el contenido de la posición solicitada
+        return arrayList[indexRow][indexColumn]
+    }
+
+    private int getRenglonNorte(renglon) {
+        //Definición de la variable auxiliar
+        def indexRow
+
+        //Si se encuentra en el primer renglón
+        if (renglon == 0){
+            indexRow = alto - 1
+        } else {
+            indexRow = renglon - 1
+        }
+
+        //Se regresa el valor calculado.
+        return indexRow
+    }
+
+    private int getColumnaOeste(columna) {
+        //Definición de la variable auxiliar
+        def indexColumn
+
+        //Si se encuentra en la última columna
+        if (columna == 0){
+            indexColumn = ancho - 1
+        } else {
+            indexColumn = columna - 1
+        }
+
+        //Se regresa el contenido de la posición solicitada
+        return indexColumn
+    }
+
+    private int getColumnaEste(columna) {
+        //Definición de la variable auxiliar
+        def indexColumn
+
+        //Si se encuentra en la última columna
+        if (columna == (ancho - 1)){
+            indexColumn = 0
+        } else {
+            indexColumn = columna + 1
+        }
+
+        //Se regresa el contenido de la posición solicitada
+        return indexColumn
+    }
+
+    private int getRenglonSur(renglon) {
+        //Definición de la variable auxiliar
+        def indexRow
+
+        //Si se encuentra en el primer renglón
+        if (renglon == (alto - 1)){
+            indexRow = 0
+        } else {
+            indexRow = renglon + 1
+        }
+
+        //Se regresa el valor calculado.
+        return indexRow
+    }
+
     //Pruebas de los métodos de la clase
     static void main(args) {
         //Se crea una semilla
         def service = new SemillaService()
-        def semilla = service.createSemillaBlock()
+        //def semilla = service.createSemillaBlock()
+        //def semilla = service.createSemillaBlinker()
+        def semilla = service.createSemillaToad()
 
         //Se crea un tablero con la semilla recien creada
         def tablero = new Tablero(semilla)
-        print tablero
+        println tablero
+
+        //Se prueban dos iteraciones de cálculos
+        tablero.calcularNuevoEstado()
+        println tablero
+        tablero.calcularNuevoEstado()
+        println tablero
     }
 }
